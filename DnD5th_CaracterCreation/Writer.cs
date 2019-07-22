57f0a9b7d5d93a2663449d52861357fcf0a922eb
SubaruSama <andresfauque@gmail.com>
2019-07-22 20:15:03 -0300

    Fixing

--- DnD5th_CaracterCreation/Writer.cs
+++ DnD5th_CaracterCreation/Writer.cs
@@ -7,12 +7,6 @@ namespace DnD5th_CaracterCreation
 {
     public class Writer
     {
-        /*
-         * Get the path of the file;
-         * Open the file;
-         * Write the contents of the character class, race and class, to file;
-         * Close the file.
-         */
         public void WriteToFile(string filename, string data)
         {
             File.WriteAllText($"{$@"{Directory.GetCurrentDirectory()}\{filename}.txt"}", data);
