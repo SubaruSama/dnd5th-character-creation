a1814c04563e2355a0bd573cab6b40f015828fbf
SubaruSama <andresfauque@gmail.com>
2019-07-23 20:52:20 -0300

    Agora o usuário tem a chance de decidir quando irá sair do programa.

--- DnD5th_CaracterCreation/Program.cs
+++ DnD5th_CaracterCreation/Program.cs
@@ -7,38 +7,44 @@ namespace DnD5th_CaracterCreation
     {
         static void Main(string[] args)
         {
-            string raceTemp, classTemp, filename, writeToText;
-            CharacterCreation character = new CharacterCreation();
-            Writer writer = new Writer();
+            bool done = false;
+            while (!done)
+            {
+                string raceTemp, classTemp, filename, writeToText;
+                CharacterCreation character = new CharacterCreation();
+                Writer writer = new Writer();
 
-            Console.WriteLine("Enter the name of the file to save: ");
-            filename = Console.ReadLine();
+                Console.WriteLine("Enter the name of the file to save: ");
+                filename = Console.ReadLine();
 
-            Console.WriteLine("Possible races for the character: ");
-            character.ReturnAllRaces();
+                Console.WriteLine("Possible races for the character: ");
+                character.ReturnAllRaces();
 
-            Console.WriteLine("\n======================\n");
+                Console.WriteLine("\n======================\n");
 
-            Console.WriteLine("Possible classes for the character: ");
-            character.ReturnAllClasses();
+                Console.WriteLine("Possible classes for the character: ");
+                character.ReturnAllClasses();
 
-            Console.Write("Enter the race of the character: ");
-            raceTemp = Console.ReadLine();
-            character.SetRace(raceTemp);
+                Console.Write("Enter the race of the character: ");
+                raceTemp = Console.ReadLine();
+                character.SetRace(raceTemp);
 
-            Console.Write("Enter the class of the character: ");
-            classTemp = Console.ReadLine();
-            character.SetClass(classTemp);
+                Console.Write("Enter the class of the character: ");
+                classTemp = Console.ReadLine();
+                character.SetClass(classTemp);
 
-            Console.WriteLine("\n======================\n");
+                Console.WriteLine("\n======================\n");
 
-            Console.WriteLine($"Info of the character: \nRace: {character.GetRace()}\nClass: {character.GetClass()}");
+                Console.WriteLine($"Info of the character: \nRace: {character.GetRace()}\nClass: {character.GetClass()}");
 
-            writeToText = $"{character.GetRace()},{character.GetClass()}";
+                writeToText = $"{character.GetRace()},{character.GetClass()}";
+                writer.WriteToFile(filename, writeToText);
 
-            writer.WriteToFile(filename, writeToText);
-
-            Console.Read();
+                Console.Write("Do you want to exit the creation?[Y/N]");
+                if (Console.ReadLine() == "Y" || Console.ReadLine() == "y") done = true;
+                
+                Console.WriteLine("\n");
+            }
         }
     }
 }
